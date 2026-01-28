02a53f6742425beaeea308929f4b28b169c23d36
singhdharmveer311 <singhdharmveer311@gmail.com>
2026-01-28 02:04:36 +0100

    till now

---
+++ fixtures/pom-fixtures.ts
@@ -0,0 +1,39 @@
+import { TodoPage } from "../pages/todo-page.ts";
+import { test as base } from "@playwright/test";
+import {LoginPage} from "../pages/LoginPage.spec.ts"
+
+
+
+type myFixtures = {
+    todopage: TodoPage;
+    loginPage : LoginPage;
+}
+
+export const test = base.extend<myFixtures>({
+
+    // todoPage fixture 
+    todopage: async ({ page }, use) => {
+        //fixture setup
+        const todoPage = new TodoPage(page);
+        await todoPage.goto();
+        await todoPage.addToDo("ïtem1");
+        
+
+        // use the fixture value in test
+        await use(todoPage);
+
+        await todoPage.removeAll();
+    },
+
+    // Login Page Fixture Setup
+    loginPage: async({page}, use) => {
+        const loginpage = new LoginPage(page);
+
+        await loginpage.gotoOrangeHRM();
+        await loginpage.loginOrangeHRM("admin", "admin123");
+    }
+    
+});
+
+export { expect } from '@playwright/test';
+
--- tests/temp-test.spec.ts
+++ tests/temp-test.spec.ts
@@ -1,8 +1,14 @@
-import {test} from '@playwright/test';
-import { LoginPage } from '../pages/LoginPage.spec';
+import { test, expect } from '../fixtures/pom-fixtures';
+// import { LoginPage } from '../pages/LoginPage.spec';
 
-test('Temp test', async({page}) => {
-    const loginPage = new LoginPage(page);
-    await loginPage.gotoOrangeHRM();
-    await loginPage.loginOrangeHRM('Admin', 'admin123');
-})
+// test('Temp test', async({page}) => {
+//     const loginPage = new LoginPage
+//     await loginPage.gotoOrangeHRM();
+//     await loginPage.loginOrangeHRM('Admin', 'admin123');
+// })
+
+
+test("Temp test", async({todopage}) => {
+    todopage.addToDo("xxx");
+    todopage.removeAll();
+});
